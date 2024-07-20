64075884b5e66f997f10fa0537644b3af3f74f95
jhonangulo89 <jhon.angulo@finkargo.com>
2024-07-20 18:40:17 -0500

    git commit -m changes names

--- backend/app/config/database.py
+++ backend/app/config/database.py
@@ -1,14 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-# from app.models.projects import Project
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/mydatabase"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/agile_ticket_tracker"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-# Base.metadata.create_all(engine)
 
 def get_db():
     db = Session()
---
+++ sql/public.user.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.users
+(user_id, email, "password", first_name, last_name, phone_number, is_active, created_at, updated_at, last_login, profile_picture)
+VALUES('9309b62e-aaf6-43bf-9456-03628066674d'::uuid, 'jhon.angulo@finkargo.com', '$2b$12$y9RLQqgLC51Ib3mJKzNxCuXPovrKN.ogkai66EhXvzeO7RQJaKjvq', 'jhon manuel', 'angulo moncada', '3001234566', true, '2024-07-20 18:09:08.919', '2024-07-20 18:09:08.919', NULL, NULL);
